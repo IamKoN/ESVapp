35b90ada887691f36c8ad7ff6a78826898379ebe
Nate Robinson <30881124+IamKingofNothing@users.noreply.github.com>
2019-01-18 19:49:35 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
+        #'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
     }
 }
 
