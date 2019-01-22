c5bd72a8f938c0481521f12da8043441d7f18558
Nate Robinson <30881124+IamKingofNothing@users.noreply.github.com>
2019-01-22 04:42:13 -0600

    Reset migrations

--- esvapp/urls.py
+++ esvapp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.SearchView.as_view(), name='search'),
     path('esvbible/', views.BookList.as_view(), name='books'),
-    path('esvbible/<str:book_name>/', views.ChapterList.as_view(), name='chapters'),
-    path('esvbible/<str:book_name>/<int:pk>/', views.VerseList.as_view(), name='verses'),
+    path('esvbible/<str:name>/', views.ChapterList.as_view(), name='chapters'),
+    path('esvbible/<str:name>/<int:pk>/', views.VerseList.as_view(), name='verses'),
     #path('esvbible/<str:book>/<int:pk>/<int:pk>/', views.VerseDetail.as_view(), name='verse_detail'),
 ]
